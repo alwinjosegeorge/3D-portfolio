d62c06753760d0a23463965869bf683958efada2
NikkitaAnnaGeroge <nikkita@gmail.com>
2026-01-13 22:43:19 +0530

    Configure Resend API key

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -3,7 +3,7 @@ import { config } from "@/data/config";
 import { Resend } from "resend";
 import { z } from "zod";
 
-const resend = new Resend(process.env.RESEND_API_KEY || "re_123");
+const resend = new Resend(process.env.RESEND_API_KEY || "re_7fDcmKYa_6k6kMCFVLzpXiBK7rJxcvkZY");
 
 const Email = z.object({
   fullName: z.string().min(2, "Full name is invalid!"),
