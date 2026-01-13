602b06b23efd1cab0e44cc00f86e0fda8359c6b6
Alwin Jose George <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2026-01-14 00:18:59 +0530

    Added resume.pdf and updated local link

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -82,9 +82,7 @@ const HeroSection = () => {
               </div>
               <div className="mt-8 flex flex-col gap-3 w-fit">
                 <Link
-                  href={
-                    "https://drive.google.com/file/d/1O97WCk2DrO9x6SHOqf7LvRbmHkMgGIb4/view?usp=sharing"
-                  }
+                  href={"/resume.pdf"}
                   target="_blank"
                   className="flex-1"
                 >
