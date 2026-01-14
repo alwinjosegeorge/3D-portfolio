688b559339d2b827303717aa505dd0264640924c
Alwin Jose George <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2026-01-14 07:57:10 +0530

    Restored Hire Me button without tooltip

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -95,6 +95,14 @@ const HeroSection = () => {
                 </Link>
                 <div className="md:self-start flex gap-3">
 
+                  <Link href={"#contact"}>
+                    <Button
+                      variant={"outline"}
+                      className="block w-full overflow-hidden"
+                    >
+                      Hire Me
+                    </Button>
+                  </Link>
                   <div className="flex items-center h-full gap-2">
 
                     <Link
